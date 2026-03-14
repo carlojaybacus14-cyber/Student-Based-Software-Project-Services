a25e9a3b90b062c6ec9308c11bb4078798228852
Carlo Jay Bacus <carlojaybacus14@gmail.com>
2026-03-14 21:39:41 +0800

    array list is finished

--- src/Danosos/ArrayListUI.java
+++ src/Danosos/ArrayListUI.java
@@ -2,12 +2,14 @@ package Danosos;
 
 import java.awt.*;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class ArrayListUI extends javax.swing.JFrame {
 
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(ArrayListUI.class.getName());
     
-    private ArrayList<Integer> arrayList;
+    private final ArrayList<Integer> arrayList;
+    private String txtContainer = "";
     
     public ArrayListUI() {
         initComponents();
@@ -20,12 +22,12 @@ public class ArrayListUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        txtOutput = new javax.swing.JTextArea();
+        btnAccess = new javax.swing.JButton();
+        btnChange = new javax.swing.JButton();
+        btnRemove = new javax.swing.JButton();
+        btnAdd = new javax.swing.JButton();
+        txtInput = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -33,36 +35,57 @@ public class ArrayListUI extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(26, 26, 26));
 
-        jTextArea1.setBackground(new java.awt.Color(17, 17, 17));
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Mongolian Baiti", 1, 14)); // NOI18N
-        jTextArea1.setForeground(new java.awt.Color(85, 85, 85));
-        jTextArea1.setRows(5);
-        jScrollPane2.setViewportView(jTextArea1);
+        txtOutput.setEditable(false);
+        txtOutput.setBackground(new java.awt.Color(17, 17, 17));
+        txtOutput.setColumns(20);
+        txtOutput.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
+        txtOutput.setForeground(new java.awt.Color(255, 255, 255));
+        txtOutput.setRows(5);
+        jScrollPane2.setViewportView(txtOutput);
 
-        jButton1.setBackground(new java.awt.Color(44, 44, 44));
-        jButton1.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(224, 224, 224));
-        jButton1.setText("Access");
+        btnAccess.setBackground(new java.awt.Color(44, 44, 44));
+        btnAccess.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
+        btnAccess.setForeground(new java.awt.Color(224, 224, 224));
+        btnAccess.setText("Access");
+        btnAccess.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAccessActionPerformed(evt);
+            }
+        });
 
-        jButton2.setBackground(new java.awt.Color(44, 44, 44));
-        jButton2.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(224, 224, 224));
-        jButton2.setText("Change");
+        btnChange.setBackground(new java.awt.Color(44, 44, 44));
+        btnChange.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
+        btnChange.setForeground(new java.awt.Color(224, 224, 224));
+        btnChange.setText("Change");
+        btnChange.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnChangeActionPerformed(evt);
+            }
+        });
 
-        jButton3.setBackground(new java.awt.Color(56, 40, 40));
-        jButton3.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(224, 136, 136));
-        jButton3.setText("Remove");
+        btnRemove.setBackground(new java.awt.Color(56, 40, 40));
+        btnRemove.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
+        btnRemove.setForeground(new java.awt.Color(224, 136, 136));
+        btnRemove.setText("Remove");
+        btnRemove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoveActionPerformed(evt);
+            }
+        });
 
-        jButton4.setBackground(new java.awt.Color(44, 44, 44));
-        jButton4.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
-        jButton4.setForeground(new java.awt.Color(224, 224, 224));
-        jButton4.setText("Add");
+        btnAdd.setBackground(new java.awt.Color(44, 44, 44));
+        btnAdd.setFont(new java.awt.Font("Mongolian Baiti", 1, 12)); // NOI18N
+        btnAdd.setForeground(new java.awt.Color(224, 224, 224));
+        btnAdd.setText("Add");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
 
-        jTextField1.setBackground(new java.awt.Color(17, 17, 17));
-        jTextField1.setFont(new java.awt.Font("Mongolian Baiti", 1, 14)); // NOI18N
-        jTextField1.setForeground(new java.awt.Color(221, 221, 221));
+        txtInput.setBackground(new java.awt.Color(17, 17, 17));
+        txtInput.setFont(new java.awt.Font("Monospaced", 1, 14)); // NOI18N
+        txtInput.setForeground(new java.awt.Color(221, 221, 221));
 
         jLabel2.setBackground(new java.awt.Color(17, 17, 17));
         jLabel2.setFont(new java.awt.Font("Mongolian Baiti", 1, 18)); // NOI18N
@@ -88,14 +111,14 @@ public class ArrayListUI extends javax.swing.JFrame {
                     .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnAccess, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(btnChange, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jTextField1, javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(btnRemove, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtInput, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(31, Short.MAX_VALUE))
@@ -108,13 +131,13 @@ public class ArrayListUI extends javax.swing.JFrame {
                 .addGap(30, 30, 30)
                 .addComponent(jLabel4)
                 .addGap(4, 4, 4)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtInput, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(btnAccess, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnChange, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRemove, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -125,6 +148,116 @@ public class ArrayListUI extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void display() {
+        txtOutput.setText(txtContainer);
+    }
+    
+    private void showErrorDialog(String message) {
+        JOptionPane.showMessageDialog(this, message, "error", JOptionPane.ERROR_MESSAGE);
+    }
+    
+    public void showReminderDialog() {
+        JOptionPane.showMessageDialog(this, "Array list is empty");
+    }
+    
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        int data;
+        try {
+            data = Integer.parseInt(txtInput.getText());
+        } catch (NumberFormatException e) {
+            showErrorDialog("Invalid user input");
+            return; 
+        }
+        
+        arrayList.add(data);
+        txtContainer += arrayList + "\n";
+        display();
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnAccessActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAccessActionPerformed
+        
+        if (arrayList.isEmpty()) {
+            showReminderDialog();
+            return;
+        }
+        
+        int index;
+        
+        try {
+            index = Integer.parseInt(txtInput.getText());
+            
+            if (index < 0 || index >= arrayList.size()) {
+                showErrorDialog("Index out of bounds");
+                return;
+            }
+        } catch (HeadlessException | NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Invalid user input", "error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        txtContainer += "Accessed element: " + arrayList.get(index) + "\n";
+        display();
+    }//GEN-LAST:event_btnAccessActionPerformed
+
+    private void btnChangeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnChangeActionPerformed
+        
+        if (arrayList.isEmpty()) {
+            showReminderDialog();
+            return;
+        }
+        
+        int data;
+        int index;
+        try {
+            data = Integer.parseInt(txtInput.getText());
+            index = Integer.parseInt(JOptionPane.showInputDialog("Enter index"));
+            
+            if (index < 0 || index >= arrayList.size()) {
+                JOptionPane.showConfirmDialog(this, "Index out of bounds", "error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        } catch (HeadlessException | NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Invalid user input", "error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        arrayList.set(index, data);
+        txtContainer += arrayList + "\n";
+        display();
+    }//GEN-LAST:event_btnChangeActionPerformed
+
+    private void btnRemoveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveActionPerformed
+        
+        if (arrayList.isEmpty()) {
+            showReminderDialog();
+            return;
+        }
+        
+        int index;
+        
+        try {
+            index = Integer.parseInt(txtInput.getText());
+            
+            if (index < 0 || index >= arrayList.size()) {
+                JOptionPane.showMessageDialog(this, "Index out of bounds", "error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Invalid user input", "error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        arrayList.remove(index);
+        
+        if (arrayList.isEmpty()) {
+            txtContainer += "Array list is now empty.\n";
+        } else {
+            txtContainer += arrayList + "\n";
+        }
+        
+        display();
+    }//GEN-LAST:event_btnRemoveActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -147,15 +280,15 @@ public class ArrayListUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton btnAccess;
+    private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnChange;
+    private javax.swing.JButton btnRemove;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField txtInput;
+    private javax.swing.JTextArea txtOutput;
     // End of variables declaration//GEN-END:variables
 }
